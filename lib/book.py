defe4cc97264a637dcf97ffdee69706b91779535
Joe <jakatsa5@gmail.com>
2024-03-13 22:24:51 +0300

    completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    """Represents a book with a title and page count."""
+
+    def __init__(self, title: str, page_count: int):
+        self.title = title
+        self._page_count = None  # Initialize _page_count attribute
+
+        # Set page_count using property setter
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    """Represents a shoe with a brand and size."""
+
+    def __init__(self, brand: str, size: int):
+        self.brand = brand
+        self._size = None  # Initialize _size attribute
+
+        # Set size using property setter
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
